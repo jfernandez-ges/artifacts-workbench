fff00a34810a2787b39601be171b1d31bfa8964e
Juan Fernández <jfernandez@greeneaglesolutions.com>
2025-10-24 12:54:22 +0200

    fix: fixed sql script

--- 2025/20251024_iberdrola_datacleaning/areleoch_script_datacleaning.sql
+++ 2025/20251024_iberdrola_datacleaning/areleoch_script_datacleaning.sql
@@ -1,16 +1,12 @@
 -- clear reanalysis event log for specific asset and date range
 delete from cscada_reanalysis_eventlog
--- select assetpath, count(*) from cscada_reanalysis_eventlog
 where startdate >=  '2025-09-28' and startdate < '2025-09-29'
 and assetpath = 'PARK_Areleoch.Gamesa.016'
 and type IN ('ARSOS')
-group by assetpath
 
 
 -- clear reanalysis common event log for specific asset and date range
-delete from cscada_reanalysis_eventlog
--- select assetpath, count(*) from cscada_reanalysis_eventlog
-where startdate >=  '2025-09-28' and startdate < '2025-09-29'
+delete from cscada_commoneventlog
+where utctimestamp >=  '2025-09-28' and utctimestamp < '2025-09-29'
 and assetpath = 'PARK_Areleoch.Gamesa.016'
-and type IN ('ARSOS')
-group by assetpath
+and alarmcode1 = 'RequestSTART' and handlerId = 'ARSOS'
