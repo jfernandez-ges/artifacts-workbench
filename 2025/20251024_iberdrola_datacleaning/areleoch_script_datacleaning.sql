9991929f47cfbbd4b0d1c42f2dde8841deeaba65
Juan Fernández <jfernandez@greeneaglesolutions.com>
2025-10-25 09:58:48 +0200

    fix: fixed dates in datacleaning script

--- 2025/20251024_iberdrola_datacleaning/areleoch_script_datacleaning.sql
+++ 2025/20251024_iberdrola_datacleaning/areleoch_script_datacleaning.sql
@@ -1,12 +1,12 @@
 -- clear reanalysis event log for specific asset and date range
 delete from cscada_reanalysis_eventlog
-where startdate >=  '2025-09-28' and startdate < '2025-09-29'
+where startdate >=  '2025-09-01' and startdate < '2025-10-30'
 and assetpath = 'PARK_Areleoch.Gamesa.016'
 and type IN ('ARSOS')
 
 
 -- clear reanalysis common event log for specific asset and date range
 delete from cscada_commoneventlog
-where utctimestamp >=  '2025-09-28' and utctimestamp < '2025-09-29'
+where utctimestamp >=  '2025-09-01' and utctimestamp < '2025-10-30'
 and assetpath = 'PARK_Areleoch.Gamesa.016'
 and alarmcode1 = 'RequestSTART' and handlerId = 'ARSOS'
